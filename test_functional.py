c9ee7df0dd3a6375f954535b9fb700df652180c1
Gary Martin <gary.martin@physics.org>
2021-06-04 01:22:32 +0100

    Playing with pytest

--- test_functional.py
+++ test_functional.py
@@ -1,9 +1,18 @@
+import pytest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+
+@pytest.fixture(scope="session")
+def browser():
+    print("\nStarting Firefox Session")
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(3)
+
+    yield driver
+    print("\nStopping Firefox Session")
+    driver.quit()
 
 
-def test_get_title():
+def test_get_title(browser):
+    browser.get('http://localhost:8000')
     assert 'success' in browser.title
-
