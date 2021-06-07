5d9aa08fc01caad67b2ed570db24cc20ab350e27
Gary Martin <gary.martin@physics.org>
2021-06-07 03:47:03 +0100

    Add post_detail view and link from post_list view

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -10,6 +10,30 @@ def user_alice():
     return User.objects.create_user("Alice")
 
 
+@pytest.fixture
+def post1(user_alice):
+    title = 'My first post'
+    text = "We were happy in those days, though we were poor."
+    post = Post.objects.create(
+        author=user_alice,
+        title=title,
+        text=text,
+    )
+    return post
+
+
+@pytest.fixture
+def post2(user_alice):
+    title = 'My second post'
+    text = "Ee by gum work seven while nine down mill nah then be reet."
+    post = Post.objects.create(
+        author=user_alice,
+        title=title,
+        text=text,
+    )
+    return post
+
+
 def test_root_url_resolves_to_post_list():
     found = resolve('/')
     assert found.func == views.post_list
@@ -23,26 +47,35 @@ def test_home_page_shows_post_list(client):
 
 
 @pytest.mark.django_db
-def test_post_list_view_shows_published_items(client, user_alice):
-    title1 = 'My first post'
-    text1 = "We were happy in those days, though we were poor."
-    Post.objects.create(
-        author=user_alice,
-        title=title1,
-        text=text1,
-    ).publish()
-
-    title2 = 'My second post'
-    text2 = "Ee by gum work seven while nine down mill nah then be reet."
-    Post.objects.create(
-        author=user_alice,
-        title=title2,
-        text=text2,
-    ).publish()
+def test_post_list_view_shows_published_item_details(client, post1, post2):
+    post2.publish()
 
     response = client.get(reverse('post_list'))
+    content = response.content.decode()
     assert response.status_code == 200
-    assert title1 in response.content.decode()
-    assert text1 in response.content.decode()
-    assert title2 in response.content.decode()
-    assert text2 in response.content.decode()
+    assert post2.title in content
+    assert post2.text in content
+    assert f"href=\"{reverse('post_detail', kwargs={'pk': post2.id})}\"" in content
+
+
+@pytest.mark.django_db
+def test_post_list_view_hides_unpublished_item_details(client, post1, post2):
+    post2.publish()
+
+    response = client.get(reverse('post_list'))
+    content = response.content.decode()
+    assert response.status_code == 200
+    assert post1.title not in content
+    assert post1.text not in content
+    assert f"href=\"{reverse('post_detail', kwargs={'pk': post1.id})}\"" not in content
+
+
+@pytest.mark.django_db
+def test_post_detail_view_hides_unpublished_item_details(client, post1):
+    post1.publish()
+
+    response = client.get(reverse('post_detail', kwargs={'pk': post1.id}))
+    content = response.content.decode()
+    assert response.status_code == 200
+    assert post1.title in content
+    assert post1.text in content
